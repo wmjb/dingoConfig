73d9c6b7e5006b236a4c854f0c256c97f58ff15c
Cory Grant <cory.grant11@gmail.com>
2025-10-24 21:48:00 -0400

    remove old files, add blank project

---
+++ dingoConfig/WeatherForecast.cs
@@ -0,0 +1,12 @@
+namespace dingoConfig;
+
+public class WeatherForecast
+{
+    public DateOnly Date { get; set; }
+
+    public int TemperatureC { get; set; }
+
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+    public string? Summary { get; set; }
+}
